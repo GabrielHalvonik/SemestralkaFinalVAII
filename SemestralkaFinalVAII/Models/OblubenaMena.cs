ce2a67347669cb7f8f29e730e45f28dda5cac4e9
gabrielhalvonik <halvonikgabriel@gmail.com>
2021-01-14 21:31:35 +0100

    Adding Favourite currency support.

---
+++ SemestralkaFinalVAII/Models/OblubenaMena.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace SemestralkaFinalVAII.Models {
+
+    public class OblubenaMena {
+        [ForeignKey("id")]
+        public string Id { get; set; }
+    }
+}
---
+++ SemestralkaFinalVAII/Models/ZoznamOblubenych.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SemestralkaFinalVAII.Models {
+
+    public class ZoznamOblubenych {
+
+        [ForeignKey("UserId")]
+        public string UserId { get; set; }
+
+        public List<OblubenaMena> Oblubene { get; set; }
+    }
+}
