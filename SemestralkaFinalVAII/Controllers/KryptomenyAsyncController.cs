5580a5cecebb3d9757fa1a809b19c6e0751f502a
gabrielhalvonik <halvonikgabriel@gmail.com>
2021-01-19 18:38:50 +0100

    Adding AJAX support for Put and Delete operations with Controller API.

---
+++ SemestralkaFinalVAII/Controllers/KryptomenyAsyncController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SemestralkaFinalVAII.Data;
+using SemestralkaFinalVAII.Models;
+
+namespace SemestralkaFinalVAII.Controllers {
+    [Route("api/[controller]")]
+
+    public class KryptomenyAsyncController : Controller {
+    private readonly ApplicationDbContext Context;
+
+        public KryptomenyAsyncController(ApplicationDbContext context) {
+            Context = context;
+        }
+
+        [HttpPut("{id}")]
+        public void Put(string id) {
+            string UserID = Context.Users.SingleOrDefault(p => p.Email == User.Identity.Name)?.Id;
+            Console.WriteLine(id + " " + UserID + "\n");
+
+            if (UserID == null) {
+                return;
+            }
+
+            if (!Context.Oblubene.Any(temp => temp.UserId == UserID)) {
+                Context.Oblubene.Add(new ZoznamOblubenych() {
+                    UserId = UserID,
+                    Oblubene = new List<string>()
+                });
+                Context.SaveChanges();
+            }
+
+            ZoznamOblubenych meny = Context.Oblubene.Single(temp => temp.UserId == UserID);
+
+            if (!meny.Oblubene.Contains(id)) {
+                meny.Oblubene.Add(id);
+            } else {
+                meny.Oblubene.Remove(id);
+            }
+            Context.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+
+        }
+
+        //[HttpGet()]
+        //public string Get() {
+        //    return "pass";
+        //}
+    }
+}
