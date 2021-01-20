d898658d0d598b0ce1c4626c3622e900d07ff0c4
gabrielhalvonik <halvonikgabriel@gmail.com>
2021-01-20 03:15:16 +0100

    refactoring...

---
+++ SemestralkaFinalVAII/Controllers/ZmenarneController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SemestralkaFinalVAII.Data;
+using SemestralkaFinalVAII.Models;
+using SemestralkaVAII.Models;
+
+namespace SemestralkaFinalVAII.Controllers {
+
+    public class ZmenarneController : Controller {
+        private readonly ApplicationDbContext Context;
+
+        public ZmenarneController(ApplicationDbContext context) {
+            Context = context;
+        }
+
+        public IActionResult Index() {
+            List<ZmenarenData> kryptomenyList = Context.Zmenarne.ToList();
+            return View(kryptomenyList.OrderBy(p => p.TrustScoreRank).ToList());
+        }
+
+        [HttpGet]
+        public void ReloadFromApi() {
+            Context.Kryptomeny.RemoveRange(Context.Kryptomeny);
+            const string url = "https://api.coingecko.com/api/v3/exchanges";
+            WebClient client = new WebClient();
+            string str = client.DownloadString(url);
+            try {
+                List<ZmenarenData> list = JsonSerializer.Deserialize<List<ZmenarenData>>(str);
+                Context.Zmenarne.AddRange(list);
+                Context.SaveChanges();
+            } catch (JsonException exception) {
+                Console.WriteLine(exception.ToString());
+            }
+        }
+    }
+}
---
+++ SemestralkaFinalVAII/Models/ZmenarenData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace SemestralkaVAII.Models {
+
+    public class ZmenarenData {
+
+        [Key]
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("year_established")]
+        public long? YearEstablished { get; set; }
+
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+
+        [JsonPropertyName("url")]
+        public Uri Url { get; set; }
+
+        [JsonPropertyName("image")]
+        public Uri Image { get; set; }
+
+        [JsonPropertyName("trust_score")]
+        public long TrustScore { get; set; }
+
+        [JsonPropertyName("trust_score_rank")]
+        public long TrustScoreRank { get; set; }
+
+        [JsonPropertyName("trade_volume_24h_btc")]
+        public double TradeVolume24HBtc { get; set; }
+
+        [JsonPropertyName("trade_volume_24h_btc_normalized")]
+        public double TradeVolume24HBtcNormalized { get; set; }
+    }
+}
